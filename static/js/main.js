970603d424d074751092135454c228c3284c915e
HENRY-2016 <jacob2016henry@gmail.com>
2021-01-24 18:05:03 +0300

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -1,40 +1,15 @@
 
-// 192.168.43.23
 
 
-function CreateCustomAlertBox()
+function LoadInterfacePage ()
 {
-    this.render = function(dialog){
-        let winW = window.innerWidth;
-        let winH = window.innerHeight;
-        let AlertBoxDisplayArea = document.getElementById('alert-box-display-area');
-        let HtmlAlertBox = document.getElementById('alert-box');
-        AlertBoxDisplayArea.style.display = "block";
-        AlertBoxDisplayArea.style.height = winH+"px";
-        HtmlAlertBox.style.left = (winW/2) - (550 * .5)+"px";
-        HtmlAlertBox.style.top = "100px";
-        HtmlAlertBox.style.display = "block";
-        document.getElementById('alert-box-header').innerHTML = "Network Error ";
-        document.getElementById('alert-box-body').innerHTML = dialog;
-        document.getElementById('alert-box-footer').innerHTML = '<button class="ok-alert-btn" onclick="Alert.ok()">OK</button>';
-    }
-    this.ok = function()
+    password = document.getElementById('password').value;
+
+    if (!(password == "Maria91"))
+        {document.getElementById('invalid-label').style.display='block';}
+    else 
         {
-            document.getElementById('alert-box').style.display = "none";
-            document.getElementById('alert-box-display-area').style.display = "none";
+            window.location="interface.html";
         }
+
 }
-var Alert = new CreateCustomAlertBox();
-
-function ShowAlertBox (){Alert.render('<center>Please turn on <br> your network <br>and <br> Try Again</center>')}
-
-
-// Beiwa
-function LoadSusanUsingGitHub ()
-{
-    if(!navigator.onLine) {CreateCustomAlertBox ()}
-    if(navigator.onLine){window.location='https://henry-2016.github.io/susan-admin-server-repo/'}
-    
-}
-
-
